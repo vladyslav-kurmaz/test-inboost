908219c59e422a0dc325ed6fb738d5940403f1d6
vladyslav-kurmaz <posolkoronovan@gmail.com>
2023-05-17 01:35:33 +0300

    add cors header

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://test-inboost.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
