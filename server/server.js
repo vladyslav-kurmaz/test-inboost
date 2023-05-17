5213d00b7c1bca10fc6b23ec4253d65e9fed03fe
vladyslav-kurmaz <posolkoronovan@gmail.com>
2023-05-17 11:33:56 +0300

    change server config

--- server/server.js
+++ server/server.js
@@ -16,8 +16,10 @@ app.use(cors({
   origin: '*'
 }));
 
+// https://test-inboost.vercel.app
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://test-inboost.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
