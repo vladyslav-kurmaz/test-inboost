59e95100762b0a7ff7958f683c9f82ea0e02267d
vladyslav-kurmaz <posolkoronovan@gmail.com>
2023-06-02 15:01:24 +0300

    add validation to login adn registration form

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { v4 as uuidv4 } from 'uuid';
 import { Link } from "react-router-dom";
 
 import NotesService from "../../service/NotesServices";
 import "./Login.scss";
+import eyeIcon from '../../image/eye.webp'
 
 const Login = ({login}) => {
     const {userLog, requestStatus, singUp} = NotesService();
-    const {status} = useSelector((state) => state.notes)
+    const {status} = useSelector((state) => state.notes);
+    const [showPass, setShowPass] = useState(false);
+    const [disableBtn, setDisableBtn] = useState(true);
 
     const [newUser, setNewUser] = useState({
         id: '',
@@ -23,9 +26,35 @@ const Login = ({login}) => {
         pass: ''
     })
 
+    useEffect(() => {
+        if (newUser.pass.length > 3 || userLogin.pass.length > 3) {
+            setDisableBtn(false);
+        } else {
+            setDisableBtn(true);
+        }
+    }, [newUser.pass, userLogin.pass])
+
+    const styleFormInput = (atr, value) => {
+        switch (atr) {
+            case 'name':
+                return value?.length > 3 || value?.length === 0 ? {} : {'border': '1px solid red'};
+            case 'surname':
+                return value?.length > 3 || value?.length === 0 ? {} : {'border': '1px solid red'};
+            case 'mail':
+                // eslint-disable-next-line
+                return value?.match('^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$') || value.length === 0 ? {} : {'border': '1px solid red'};
+            case 'pass':
+                
+                return value?.length > 3 || value.length === 0 ? {} : {'border': '1px solid red'};
+            default:
+                return ;
+        }
+    }
+
     const addInfoNewUser = (e) => {
         switch (e.target.getAttribute('id')) {
             case 'name':
+                styleFormInput('name', userLogin.name)
                 return setNewUser((state) => { 
                     return{
                     ...newUser,
@@ -83,20 +112,36 @@ const Login = ({login}) => {
                 onSubmit={(e) => userLog(e, userLogin)}>
                 <input 
                     className="auto__form-login " 
-                    type="text" 
+                    type="email" 
                     id='mail' 
                     placeholder="Пошта"
                     onChange={enterUserInfo}
+                    required
+                    style={styleFormInput('mail', userLogin.mail)}
                     />
-                <input 
-                    className="auto__form-pass " 
-                    type="text" 
-                    id='pass' 
-                    placeholder="Пароль"
-                    onChange={enterUserInfo}/>
+
+                <label htmlFor="pass">
+                    <input 
+                        className="auto__form-pass " 
+                        type={showPass ? 'text' : 'password'} 
+                        id='pass' 
+                        placeholder="Пароль"
+                        onChange={enterUserInfo}
+                        required
+                        style={styleFormInput('pass', userLogin.pass)}/>
+                
+                    <img 
+                        src={eyeIcon} 
+                        className="auto__form-eye" 
+                        alt="eye"
+                        onClick={() => setShowPass(() => !showPass)} />
+                </label>
+                
+
 
                 <button 
-                    className="auto__form-enter">Увійти</button>
+                    className="auto__form-enter"
+                    disabled={disableBtn}>Увійти</button>
                 {requestStatus(status, 'Акаунт з таким логіном та паролем не знайдено!')}         
             </form>
 
@@ -119,29 +164,49 @@ const Login = ({login}) => {
                     type="text" 
                     id='name' 
                     placeholder="І'мя"
-                    onChange={addInfoNewUser}/>
+                    onChange={addInfoNewUser}
+                    required
+                    style={styleFormInput('name', newUser.name)}/>
                 <input 
                     className="auto__form-login " 
                     type="text" 
                     id='surname' 
                     placeholder="Прізвище"
-                    onChange={addInfoNewUser}/>
+                    onChange={addInfoNewUser}
+                    required
+                    style={styleFormInput('surname', newUser.surname)}/>
                 <input 
                     className="auto__form-login " 
-                    type="text" 
+                    type="email" 
                     id='mail' 
                     placeholder="Логін"
-                    onChange={addInfoNewUser}/>
-                <input 
-                    className="auto__form-pass " 
-                    type="text" 
-                    id='pass' 
-                    placeholder="Пароль"
-                    onChange={addInfoNewUser}/>
+                    onChange={addInfoNewUser}
+                    required
+                    style={styleFormInput('mail', newUser.mail)}/>
+                
+
+                <label htmlFor="pass">
+
+                    <input 
+                        className="auto__form-pass " 
+                        type={showPass ? 'text' : 'password'}  
+                        id='pass' 
+                        placeholder="Пароль"
+                        onChange={addInfoNewUser}
+                        required
+                        style={styleFormInput('pass', newUser.pass)}/>
+                    <img 
+                        src={eyeIcon} 
+                        className="auto__form-eye" 
+                        alt="eye"
+                        onClick={() => setShowPass(() => !showPass)} />
+                </label>    
+                
 
                 <button
                     // to='/notes'
                     className="auto__form-enter"
+                    disabled={disableBtn}
                     >Зареєструватись</button>
 
                 {requestStatus(status, 'Акаунт з таким логіном і паролем вже зареєстровано')}
